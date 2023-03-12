7d9e31aa19f7f1e06a0c99e8893d799d0a8ad77d
wasdx19 <vanrwei@gmail.com>
2023-03-12 13:14:02 +0600

    HeaderInterceptor

--- app/src/main/java/com/example/whatsappclone/network/ApiClient.kt
+++ app/src/main/java/com/example/whatsappclone/network/ApiClient.kt
@@ -1,6 +1,8 @@
 package com.example.whatsappclone.network
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -15,9 +17,14 @@ class ApiClient {
     private var retrofit: Retrofit? = null
 
     fun getClient(): Retrofit? {
+        val headerInterceptor = HeaderInterceptor()
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.BODY
-        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .addInterceptor(headerInterceptor)
+            .build()
 
         retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
@@ -25,5 +32,18 @@ class ApiClient {
             .client(client)
             .build()
         return retrofit
+    }
+}
+
+class HeaderInterceptor: Interceptor{
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val origin = chain.request()
+        val request = origin.newBuilder()
+            .addHeader("Authorization", "")
+            .addHeader("Content-Type", "application/json")
+            .addHeader("Accept-Language", "")
+            .build()
+
+        return chain.proceed(request)
     }
 }
--- app/src/main/java/com/example/whatsappclone/network/ApiServices.kt
+++ app/src/main/java/com/example/whatsappclone/network/ApiServices.kt
@@ -2,6 +2,7 @@ package com.example.whatsappclone.network
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiServices {
 
